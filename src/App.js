c1bd2bd0beeb3b4622721154e9cbabf73f606b77
Totowdu80 <60933941+Totowdu80@users.noreply.github.com>
2020-03-31 15:53:48 +0200

    Test Commit
    Test commit from Sandbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
-import Contact from './components/Contact'
+import React from "react";
+import "./App.css";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Contact img="https://randomuser.me/api/portraits/women/14.jpg" firstName="Mabel" lastName="Bishop" />
+      <Contact
+        avatar="https://randomuser.me/api/portraits/women/14.jpg"
+        name="Mabel Bishop"
+        online="online"
+      />
+      <Contact
+        avatar="https://randomuser.me/api/portraits/men/72.jpg"
+        name="Bernard Stevens"
+        online="offline"
+      />
+      <Contact
+        avatar="https://randomuser.me/api/portraits/women/47.jpg"
+        name="Marily Wagner"
+        online="offline"
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import "./Contact.css"
+import React from "react";
+import "./Contact.css";
 
-const Contact = ({ firstName, lastName, img }) => {
-
-    return (
-        <div className="Contact">
-            <img className="avatar" src={img} />
-            <div>
-                <h2 className ="name">{firstName} {lastName}</h2>
-                <div className="status">
-                    <div className ="status-online"></div>
-                    <div className ="status-text">online</div>
-                </div>
-            </div>
-            
+const Contact = ({ name, avatar, online }) => {
+  return (
+    <div className="Contact">
+      <img className="avatar" src={avatar} alt="Avatar" />
+      <div>
+        <h2 className="name">{name}</h2>
+        <div className="status">
+          <div
+            className={online === "online" ? "status-online" : "status-offline"}
+          />
+          <div className="status-text">{online}</div>
         </div>
-    )
-}
-   
+      </div>
+    </div>
+  );
+};
+
 export default Contact;
