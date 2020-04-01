d4bb6e05e3f6dbca63755e52d3a1d1c5ae22b842
Totowdu80 <totowdu80@gmail.com>
2020-04-01 14:33:40 +0200

    Editing ContactList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
 import "./App.css";
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/14.jpg"
-        name="Mabel Bishop"
-        online="online"
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/men/72.jpg"
-        name="Bernard Stevens"
-        online="offline"
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/47.jpg"
-        name="Marily Wagner"
-        online="offline"
+      <ContactList/>
       />
     </div>
   );
