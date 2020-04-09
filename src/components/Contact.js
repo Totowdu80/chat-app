f32d3dc6ab0919fcf02b0542cb39b9c0822f8d4c
Totowdu80 <totowdu80@gmail.com>
2020-04-09 16:05:30 +0200

    Transforming function to class

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,43 @@
 import React from "react";
 import "./Contact.css";
 
-const Contact = ({ name, avatar, online }) => {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={avatar} alt="Avatar" />
-      <div>
-        <h2 className="name">{name}</h2>
-        <div className="status">
-          <div
-            className={online === "online" ? "status-online" : "status-offline"}
-          />
-          <div className="status-text">{online}</div>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: this.props.online
+    };
+  }
+
+  render() {
+    return (
+      <div className="Contact">
+        <img src={this.props.avatar} className="avatar" alt="" />
+        <div>
+          <h4 className="name"> {this.props.name} </h4>
+          <div className="status">
+            <div
+              onClick={event => {
+                const newStatus = !this.state.online;
+                this.setState({ online: newStatus });
+              }}
+            >
+              {this.state.online ? (
+                <div className="status-online" />
+              ) : (
+                <div className="status-offline" />
+              )}
+              {this.state.online ? (
+                <div className="status-text">online </div>
+              ) : (
+                <div className="status-text">offline </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Contact;
